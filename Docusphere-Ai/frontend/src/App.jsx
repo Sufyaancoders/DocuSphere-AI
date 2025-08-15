23a6dc17b34e453cc78ef9b958064607379f4e87
sufyaan ahmad <sufyaanahmadx9x@gmail.com>
2025-08-15 23:38:42 +0530

    Resolve Refactor App component structure

--- Docusphere-Ai/frontend/src/App.jsx
+++ Docusphere-Ai/frontend/src/App.jsx
@@ -5,11 +5,7 @@ function App() {
   return (
     <>
       {/* Tailwind test element */}
-      <div className="text-center text-2xl font-bold mt-10">
-        <div className="mt-8 p-6 bg-gradient-to-r from-blue-500 to-purple-500 text-red-500 text-2xl font-bold rounded-lg shadow-lg text-center">
-          If you see this box styled, Tailwind CSS is working!
-        </div>
-      </div>
+     
       <div>
         <Login />
       </div>
