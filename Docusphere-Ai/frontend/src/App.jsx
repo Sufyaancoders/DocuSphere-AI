d50e72ee4dc724c4d051ef37e47d3a0ae2db7477
sufyaan ahmad <sufyaanahmadx9x@gmail.com>
2025-08-15 17:21:11 +0530

    Remove @tailwindcss/oxide package and its associated files
    - Deleted the index.js file, package.json, and install script from the @tailwindcss/oxide package.
    - Removed the LICENSE file for the @tailwindcss/postcss package.

--- Docusphere-Ai/frontend/src/App.jsx
+++ Docusphere-Ai/frontend/src/App.jsx
@@ -1,27 +1,20 @@
-
 import React from 'react';
 import Login from './pages/login';
+
 function App() {
   return (
-<<<<<<< HEAD
     <>
-      <div>
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-        
-      </div>
       {/* Tailwind test element */}
       <div className="text-center text-2xl font-bold mt-10">
-      <div className="mt-8 p-6 bg-gradient-to-r from-blue-500 to-purple-500 text-red-500 text-2xl font-bold rounded-lg shadow-lg text-center">
-        If you see this box styled, Tailwind CSS is working!
+        <div className="mt-8 p-6 bg-gradient-to-r from-blue-500 to-purple-500 text-red-500 text-2xl font-bold rounded-lg shadow-lg text-center">
+          If you see this box styled, Tailwind CSS is working!
+        </div>
       </div>
+      <div>
+        <Login />
       </div>
     </>
-  )
-=======
-    <div>
-<Login />
-    </div>
   );
->>>>>>> 7664e0d6543f249495f123ae8d8eb66d2ea3adee
 }
-export default App; 
+
+export default App;
