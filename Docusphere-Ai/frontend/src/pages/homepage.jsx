f7cdd14e70cf2becc0fdff91a1f648ab09154f94
sufyaan ahmad <sufyaanahmadx9x@gmail.com>
2025-08-16 00:09:44 +0530

    feat: add Features and AIBackground components to enhance homepage with AI capabilities

--- Docusphere-Ai/frontend/src/pages/homepage.jsx
+++ Docusphere-Ai/frontend/src/pages/homepage.jsx
@@ -2,12 +2,14 @@ import React, { StrictMode } from 'react';
 import HeroPlace from '../components/heroSection.jsx';
 import Navbar from '../components/navbar';
 import HowItWorks from '../components/HowItWorks.jsx';
+import Features from '../components/Features.jsx';
 const Home = () => {
   return (
     <div className='min-h-screen'>
       <Navbar />
       <HeroPlace />
       <HowItWorks />
+        <Features />
     </div>
   );
 }
