fabeb7c0e998443aa17ba989784433978d97538c
sufyaan ahmad <sufyaanahmadx9x@gmail.com>
2025-08-15 23:20:33 +0530

    feat: add HowItWorks component to explain the process and integrate it into the homepage

--- Docusphere-Ai/frontend/src/pages/homepage.jsx
+++ Docusphere-Ai/frontend/src/pages/homepage.jsx
@@ -1,12 +1,13 @@
 import React, { StrictMode } from 'react';
 import HeroPlace from '../components/heroSection.jsx';
 import Navbar from '../components/navbar';
-
+import HowItWorks from '../components/HowItWorks.jsx';
 const Home = () => {
   return (
     <div className='min-h-screen'>
       <Navbar />
       <HeroPlace />
+      <HowItWorks />
     </div>
   );
 }
